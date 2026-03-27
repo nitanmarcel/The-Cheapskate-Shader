7e7ac62c5c59e5f22f45b7d137a8f7e95ac8b620
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2026-03-27 22:26:21 +0200

    refactor: separate settings.glsl

---
+++ shaders/lib/settings.glsl
@@ -0,0 +1,5 @@
+const int   shadowMapResolution = 2048;  // Shadow map resolution [512 1024 2048 4096 8192]
+
+
+#define BLOOM // Enables bloom.
+#define BLOOM_STRENGTH 0.12 // Bloom Birghtness [0.027 0.036 0.045 0.054 0.063 0.072 0.081 0.09 0.10 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.21 0.23 0.25 0.28 0.32 10.00]0.12
--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -1,7 +1,6 @@
 #version 330 compatibility
 
-#define BLOOM // Enables bloom.
-#define BLOOM_STRENGTH 0.12 // Bloom Birghtness [0.027 0.036 0.045 0.054 0.063 0.072 0.081 0.09 0.10 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.21 0.23 0.25 0.28 0.32 10.00]0.12
+#include "../lib/settings.glsl"
 
 #ifdef VERTEX_SHADER
 
@@ -26,18 +25,20 @@ layout(location = 0) out vec4 color;
 void main() {
 	color = texture(colortex0, texcoord);
 
-	vec3 blur = texture2D(colortex1, texcoord / 4.0).rgb;
-	blur = clamp(blur, vec3(0.0), vec3(1.0));
-	blur *= blur;
+	#ifdef BLOOM
+		vec3 blur = texture2D(colortex1, texcoord / 4.0).rgb;
+		blur = clamp(blur, vec3(0.0), vec3(1.0));
+		blur *= blur;
 
-	float luminance = dot(blur, vec3(0.2126, 0.7152, 0.0722));
-	float threshold = 0.7;
-	float softThreshold = 0.1;
+		float luminance = dot(blur, vec3(0.2126, 0.7152, 0.0722));
+		float threshold = 0.7;
+		float softThreshold = 0.1;
 
-	float brightness = smoothstep(threshold - softThreshold, threshold + softThreshold, luminance);
-	blur *= brightness;
+		float brightness = smoothstep(threshold - softThreshold, threshold + softThreshold, luminance);
+		blur *= brightness;
 
-	float bloomStrength = BLOOM_STRENGTH * 0.08;
-	color.rgb = mix(color.rgb, blur, bloomStrength);
+		float bloomStrength = BLOOM_STRENGTH * 0.08;
+		color.rgb = mix(color.rgb, blur, bloomStrength);
+	#endif // BLOOM
 }
 #endif // FRAGMENT_SHADER
